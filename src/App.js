75f45a2acd156296e4a1d0b3c1cb997f681f1a07
Seth Fitzsimmons <seth@mojodna.net>
2017-06-02 22:52:14 -0700

    Nav bar

--- src/App.js
+++ src/App.js
@@ -15,15 +15,52 @@ FocusStyleManager.onlyShowFocusOnTabs();
 class App extends Component {
   render() {
     return (
-      <Sidebar>
-        <HomePanel />
-        <AOIPanel />
-        <OpenDroneMapPanel />
-        <ImageryPanel />
-        <DownloadsPanel />
-        <AdminPanel />
-        <SettingsPanel />
-      </Sidebar>
+      <div>
+        {/* TODO extract navbar */}
+        <nav className="pt-navbar pt-fixed-top pt-dark">
+          <div className="pt-navbar-group pt-align-left">
+            <div className="pt-navbar-heading">POSM</div>
+          </div>
+          <div className="pt-navbar-group pt-align-right">
+            <a
+              href="/omk/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="pt-button pt-minimal pt-icon-mobile-phone"
+            >
+              OMK
+            </a>
+            <a
+              href="/fp/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="pt-button pt-minimal pt-icon-clipboard"
+            >
+              FP
+            </a>
+            {/* TODO don't hard-code this */}
+            <a
+              href="http://osm.posm.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="pt-button pt-minimal pt-icon-send-to-map"
+            >
+              OSM
+            </a>
+            <span className="pt-navbar-divider" />
+            <button className="pt-button pt-minimal pt-icon-cog" />
+          </div>
+        </nav>
+        <Sidebar>
+          <HomePanel />
+          <AOIPanel />
+          <OpenDroneMapPanel />
+          <ImageryPanel />
+          <DownloadsPanel />
+          <AdminPanel />
+          <SettingsPanel />
+        </Sidebar>
+      </div>
     );
   }
 }

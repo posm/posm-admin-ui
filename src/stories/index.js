89467e98d30db10e90cb55fb06c229ad1eabd846
Seth Fitzsimmons <seth@mojodna.net>
2017-06-02 17:38:52 -0700

    Blueprint-powered Card experiment

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,6 +3,11 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
+import { Col, Grid, Row } from "react-bootstrap";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+import "@blueprintjs/core/dist/blueprint.css";
 
 import Button from "./Button";
 import Welcome from "./Welcome";
@@ -17,4 +22,27 @@ storiesOf("Button", module)
   )
   .add("with some emoji", () =>
     <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
+  );
+
+storiesOf("Card", module)
+  .addDecorator(story =>
+    <Grid>
+      <Row><Col md={12}>Title</Col></Row>
+      <Row>
+        <Col md={4}>{story()}</Col>
+      </Row>
+    </Grid>
+  )
+  .add("default", () => <div className="pt-card">blah blah</div>)
+  .add("elevation-1", () =>
+    <div className="pt-card pt-elevation-1">blah blah</div>
+  )
+  .add("elevation-2", () =>
+    <div className="pt-card pt-elevation-2">blah blah</div>
+  )
+  .add("elevation-3", () =>
+    <div className="pt-card pt-elevation-3">blah blah</div>
+  )
+  .add("elevation-4", () =>
+    <div className="pt-card pt-elevation-4">blah blah</div>
   );

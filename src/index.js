691ae489d70541588ea55b6cbabd266065c79691
Seth Fitzsimmons <seth@mojodna.net>
2017-06-02 17:08:04 -0700

    Add Bootstrap and React Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));

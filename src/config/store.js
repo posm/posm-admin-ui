55b793faa709f557648b65706925170a355c7806
Seth Fitzsimmons <seth@mojodna.net>
2017-06-03 23:44:06 -0700

    HMR + support for Redux devtools extension

--- src/config/store.js
+++ src/config/store.js
@@ -1,22 +1,37 @@
 import createHistory from "history/createBrowserHistory";
 import { routerMiddleware } from "react-router-redux";
 import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 
+import * as actionCreators from "../actions";
 import reducers from "../reducers";
 
 const history = createHistory();
 
-const store = createStore(
-  reducers,
-  applyMiddleware(
-    routerMiddleware(history),
-    thunk,
-    createLogger({
-      collapsed: true
-    })
-  )
-);
+export default preloadedState => {
+  const composeEnhancers = composeWithDevTools({ actionCreators });
+  const store = createStore(
+    reducers,
+    preloadedState,
+    composeEnhancers(
+      applyMiddleware(
+        routerMiddleware(history),
+        thunk,
+        createLogger({
+          collapsed: true
+        })
+      )
+    )
+  );
 
-export default store;
+  if (process.env.NODE_ENV !== "production" && module.hot) {
+    module.hot.accept("../reducers", () => {
+      const nextReducer = require("../reducers");
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  return store;
+};
--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import "bootstrap/dist/css/bootstrap-theme.css";
 import "@blueprintjs/core/dist/blueprint.css";
 
 import App from "./App";
-import store from "./config/store";
+import configureStore from "./config/store";
 
 import "./index.css";
+
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
@@ -17,3 +19,14 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById("root")
+    );
+  });
+}

7dac5d45d292f752fc5f011409653b4cbb5ba82a
Seth Fitzsimmons <seth@mojodna.net>
2017-06-12 18:55:31 -0700

    No logs right now

--- src/components/SettingsPanel.js
+++ src/components/SettingsPanel.js
@@ -1,24 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Button as BSButton, PageHeader, Panel, Well } from "react-bootstrap";
+import { PageHeader, Panel, Well } from "react-bootstrap";
 import { Event } from "react-socket-io";
 
-import LogModal from "./LogModal";
 import NetworkSettingsForm from "./NetworkSettingsForm";
 import { updateNetworkConfig } from "../actions";
 
 class SettingsPanel extends Component {
   state = {};
-
-  showLogs = () =>
-    this.setState({
-      showLogs: true
-    });
-
-  hideLogs = () =>
-    this.setState({
-      showLogs: false
-    });
 
   submit = ({ bridged, ssid, wpa, wpa_passphrase }, dispatch) =>
     dispatch(
@@ -43,7 +32,7 @@ class SettingsPanel extends Component {
 
   render() {
     const { network: { wifi: { ssid, wpa, wpa_passphrase } } } = this.props;
-    const { running, showLogs, statusMessage } = this.state;
+    const { running, statusMessage } = this.state;
 
     const initialValues = {
       bridged: false,
@@ -57,20 +46,7 @@ class SettingsPanel extends Component {
         <Event event="network-config" handler={this.onMessage} />
         <PageHeader>
           Network Settings
-          <BSButton
-            className="pull-right"
-            bsSize="small"
-            bsStyle="warning"
-            onClick={this.showLogs}
-          >
-            Show Logs
-          </BSButton>
         </PageHeader>
-        <LogModal
-          onHide={this.hideLogs}
-          event="network-config"
-          show={showLogs}
-        />
         <Panel header="Wi-Fi">
           {running && <Well bsSize="small">{statusMessage}</Well>}
           <NetworkSettingsForm

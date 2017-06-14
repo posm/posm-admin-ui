f3698d283bcb758a44891f4d9d8e39ae7201f0f9
Seth Fitzsimmons <seth@mojodna.net>
2017-06-14 13:20:37 -0700

    Give AOIs breathing room

--- src/components/AOIFiles.js
+++ src/components/AOIFiles.js
@@ -3,13 +3,20 @@ import path from "path";
 import React from "react";
 import { Panel } from "react-bootstrap";
 
+const styles = {
+  li: {
+    marginTop: 15,
+    marginBottom: 15
+  }
+};
+
 export default ({ files, posm, style }) =>
   files != null &&
   files.length > 0 &&
   <Panel header="AOI Files">
     <ul style={style}>
       {files.map(({ contents, description, title }, idx) =>
-        <li key={`aoi-${idx}`}>
+        <li key={`aoi-${idx}`} style={styles.li}>
           <strong>{title}</strong>
           {description && <span> - <em>{description}</em></span>}
           <ul style={style}>

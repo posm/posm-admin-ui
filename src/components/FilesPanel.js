4c8c3fbf159825b4a69fd648443f8613d4aaba14
Seth Fitzsimmons <seth@mojodna.net>
2017-06-14 13:29:55 -0700

    Remove bundled downloads

--- src/components/FilesPanel.js
+++ src/components/FilesPanel.js
@@ -7,45 +7,6 @@ import { connect } from "react-redux";
 import AOIFiles from "./AOIFiles";
 import { getAOIFiles } from "../selectors";
 
-const availableDownloads = [
-  {
-    url: "downloads/ODKCollect_v1.4.1.apk",
-    name: "OpenDataKit (ODK Collect for Android)"
-  },
-  {
-    url: "downloads/OpenMapKit_v1.2.apk",
-    name: "OpenMapKit (Android)"
-  },
-  {
-    url: "downloads/jre_8u11-windows-i586.exe",
-    name: "Java JRE (32-bit Windows)"
-  },
-  {
-    url: "downloads/jre_8u11-windows-x64.exe",
-    name: "Java JRE (64-bit Windows)"
-  },
-  {
-    url: "downloads/jre_8u11-macosx-x64.dmg",
-    name: "Java JRE (macOS)"
-  },
-  {
-    url: "downloads/josm.jar",
-    name: "JOSM"
-  },
-  {
-    url: "downloads/QGIS-OSGeo4W-2.18.0-1-Setup-x86.exe",
-    name: "QGIS (32-bit Windows)"
-  },
-  {
-    url: "downloads/QGIS-OSGeo4W-2.18.0-1-Setup-x86_64.exe",
-    name: "QGIS (64-bit Windows)"
-  },
-  {
-    url: "downloads/QGIS-2.18.0-1.dmg",
-    name: "QGIS (macOS)"
-  }
-];
-
 const styles = {
   outerUL: {
     columnCount: 3,
@@ -61,21 +22,13 @@ const styles = {
 const DownloadsPanel = ({ aoiFiles, posm, publicFiles }) =>
   <div className="posm-panel">
     <PageHeader>Files</PageHeader>
-    <Panel header="System">
-      <ul style={styles.outerUL}>
-        {availableDownloads.map((x, idx) =>
-          <li key={idx}><a href={x.url}>{x.name}</a></li>
-        )}
-      </ul>
-    </Panel>
-    <AOIFiles files={aoiFiles} posm={posm} style={styles.ul} />
-    {publicFiles.length > 0 &&
-      <Panel header="Misc.">
-        <p>
-          To add to this list, copy files to
-          {" "}<a href="smb://posm/public"><code>smb://posm/public</code></a>
-          {" "}(Windows: <code>\\POSM\public</code>).
-        </p>
+    <Panel header="Public">
+      <p>
+        To add to this list, copy files to
+        {" "}<a href="smb://posm/public"><code>smb://posm/public</code></a>
+        {" "}(Windows: <code>\\POSM\public</code>).
+      </p>
+      {publicFiles.length > 0 &&
         <ul style={styles.outerUL}>
           {publicFiles.map((file, idx) =>
             <li key={idx}>
@@ -84,8 +37,9 @@ const DownloadsPanel = ({ aoiFiles, posm, publicFiles }) =>
               </a>
             </li>
           )}
-        </ul>
-      </Panel>}
+        </ul>}
+    </Panel>
+    <AOIFiles files={aoiFiles} posm={posm} style={styles.ul} />
   </div>;
 
 const mapStateToProps = state => ({

78f8302c8ea5bf0ce2661e52148731af8a6e7533
Seth Fitzsimmons <seth@mojodna.net>
2017-06-14 14:47:29 -0700

    Files info + help

--- src/components/FilesPanel.js
+++ src/components/FilesPanel.js
@@ -1,13 +1,25 @@
 import path from "path";
 
-import React from "react";
-import { PageHeader, Panel } from "react-bootstrap";
+import React, { Component } from "react";
+import { Carousel, Modal, PageHeader, Panel } from "react-bootstrap";
 import { connect } from "react-redux";
 
 import AOIFiles from "./AOIFiles";
 import { getAOIFiles } from "../selectors";
 
+import macSMBConnect1 from "../images/mac-smb-connect-1.png";
+import macSMBConnect2 from "../images/mac-smb-connect-2.png";
+import macSMBConnect3 from "../images/mac-smb-connect-3.png";
+
 const styles = {
+  carousel: {
+    backgroundColor: "#666",
+    height: 315
+  },
+  carouselImage: {
+    marginLeft: "auto",
+    marginRight: "auto"
+  },
   outerUL: {
     columnCount: 3,
     listStyle: "none",
@@ -19,28 +31,99 @@ const styles = {
   }
 };
 
-const DownloadsPanel = ({ aoiFiles, posm, publicFiles }) =>
-  <div className="posm-panel">
-    <PageHeader>Files</PageHeader>
-    <Panel header="Public">
-      <p>
-        To add to this list, copy files to
-        {" "}<a href="smb://posm/public"><code>smb://posm/public</code></a>
-        {" "}(Windows: <code>\\POSM\public</code>).
-      </p>
-      {publicFiles.length > 0 &&
-        <ul style={styles.outerUL}>
-          {publicFiles.map((file, idx) =>
-            <li key={idx}>
-              <a href={`${posm}/public${file}`}>
-                <code>{path.basename(file)}</code>
-              </a>
-            </li>
-          )}
-        </ul>}
-    </Panel>
-    <AOIFiles files={aoiFiles} posm={posm} style={styles.ul} />
-  </div>;
+class DownloadsPanel extends Component {
+  state = {
+    showModal: false
+  };
+
+  closeModal = () =>
+    this.setState({
+      showModal: false
+    });
+
+  openModal = () =>
+    this.setState({
+      showModal: true
+    });
+
+  render() {
+    const { aoiFiles, posm, publicFiles } = this.props;
+    const { showModal } = this.state;
+
+    return (
+      <div className="posm-panel">
+        <Modal show={showModal} onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Connecting</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Carousel interval={0} style={styles.carousel}>
+              <Carousel.Item>
+                <img
+                  alt="Connect to Server"
+                  style={styles.carouselImage}
+                  src={macSMBConnect1}
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  alt="Connecting..."
+                  style={styles.carouselImage}
+                  src={macSMBConnect2}
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  alt="Guest"
+                  style={styles.carouselImage}
+                  src={macSMBConnect3}
+                />
+              </Carousel.Item>
+            </Carousel>
+          </Modal.Body>
+        </Modal>
+        <PageHeader>Files</PageHeader>
+        <Panel
+          header={
+            <h4>
+              Public{" "}
+              <span
+                onClick={this.openModal}
+                className="pull-right pt-icon-standard pt-icon-help pt-intent-primary"
+              />
+            </h4>
+          }
+        >
+          <p>
+            To add to this list, copy files to
+            {" "}<a href="smb://posm/public"><code>smb://posm/public</code></a>
+            {" "}(Windows: <code>\\POSM\public</code>), connecting as Guest.
+          </p>
+          {publicFiles.length > 0 &&
+            <ul style={styles.outerUL}>
+              {publicFiles.map((file, idx) =>
+                <li key={idx}>
+                  <a href={`${posm}/public${file}`}>
+                    <code>{path.basename(file)}</code>
+                  </a>
+                </li>
+              )}
+            </ul>}
+          <p>
+            See
+            {" "}
+            <a href="https://github.com/posm/posm/wiki/Recommended-Downloads">
+              Recommended Downloads
+              {" "}<span className="pt-icon-standard pt-icon-offline" />
+            </a>
+            {" "}for a list of tools we recommend making available.
+          </p>
+        </Panel>
+        <AOIFiles files={aoiFiles} posm={posm} style={styles.ul} />
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   aoiFiles: getAOIFiles(state),

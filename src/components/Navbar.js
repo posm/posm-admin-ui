6603b0046242e61679d94b70c6f78a02ae4ed16e
Seth Fitzsimmons <seth@mojodna.net>
2017-06-14 16:58:48 -0700

    Improve behavior when an AOI is active but it's missing from the list

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,11 @@ import { Image } from "react-bootstrap";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+import { getActiveAOIName } from "../selectors";
+
 import posmLogo from "../images/posm@2x.png";
 
-const Navbar = ({ aois, osm, posm }) =>
+const Navbar = ({ activeAOI, osm, posm }) =>
   <nav className="pt-navbar pt-fixed-top pt-dark">
     <div className="pt-navbar-group pt-align-left">
       <div className="pt-navbar-heading">
@@ -14,13 +16,7 @@ const Navbar = ({ aois, osm, posm }) =>
     </div>
     <div className="pt-navbar-group pt-align-right">
       <Link to="/aois">
-        Active AOI:
-        {" "}
-        {aois.active
-          ? <strong>
-              {aois.available.find(x => x.name === aois.active).title}
-            </strong>
-          : <em>none</em>}
+        Active AOI: {activeAOI ? <strong>{activeAOI}</strong> : <em>none</em>}
       </Link>
     </div>
   </nav>;
@@ -28,7 +24,7 @@ const Navbar = ({ aois, osm, posm }) =>
 Navbar.propTypes = {};
 
 const mapStateToProps = state => ({
-  aois: state.aois,
+  activeAOI: getActiveAOIName(state),
   osm: state.osm,
   posm: state.config.posm
 });
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -15,6 +15,17 @@ export const getAOIFiles = createSelector([getAvailableAOIs], aois =>
   }))
 );
 
+export const getActiveAOI = state => state.aois.active;
+
+export const getActiveAOIName = createSelector(
+  [getAvailableAOIs, getActiveAOI],
+  (aois, activeAOI) => {
+    const active = aois.find(x => x.name === activeAOI);
+
+    return active && active.title;
+  }
+);
+
 export const getODMEndpoint = state => state.config.odmEndpoint;
 
 export const getPOSMEndpoint = state => state.config.posm;
